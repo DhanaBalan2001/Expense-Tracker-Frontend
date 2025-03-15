dcc8075a8e19e3a470055dce542cc24f0ef7a70a
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-03-15 23:25:27 +0530

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,12 +1,12 @@
 import api from './api';
 
 export const login = async (credentials) => {
-  const response = await api.post('/auth/login', credentials);
+  const response = await api.post('api/auth/login', credentials);
   return response.data;
 };
 
 export const register = async (userData) => {
-  const response = await api.post('/auth/register', userData);
+  const response = await api.post('api/auth/register', userData);
   return response.data;
 };
 
