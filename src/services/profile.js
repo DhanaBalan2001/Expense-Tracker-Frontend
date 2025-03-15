f6e5a24c9ac8bb3f60a2b66933374915abccc177
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-03-15 23:29:42 +0530

    Update profile.js

--- src/services/profile.js
+++ src/services/profile.js
@@ -1,21 +1,21 @@
 import api from './api';
 
 export const getProfile = async () => {
-  const response = await api.get('/profile');
+  const response = await api.get('/api/profile');
   return response.data;
 };
 
 export const updateProfile = async (profileData) => {
-  const response = await api.put('/profile', profileData);
+  const response = await api.put('/api/profile', profileData);
   return response.data;
 };
 
 export const changePassword = async (passwordData) => {
-  const response = await api.post('/profile/change-password', passwordData);
+  const response = await api.post('/api/profile/change-password', passwordData);
   return response.data;
 };
 
 export const deleteAccount = async (password) => {
-  const response = await api.delete('/profile', { data: { password } });
+  const response = await api.delete('/api/profile', { data: { password } });
   return response.data;
 };
