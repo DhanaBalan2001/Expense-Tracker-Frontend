1b50eae8535381dbc2917c18d4fdf98fb06ba91e
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-03-15 23:31:37 +0530

    Update settings.js

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,16 +1,16 @@
 import api from './api';
 
 export const getSettings = async () => {
-  const response = await api.get('/settings');
+  const response = await api.get('/api/settings');
   return response.data;
 };
 
 export const updateSettings = async (settingsData) => {
-  const response = await api.put('/settings', settingsData);
+  const response = await api.put('/api/settings', settingsData);
   return response.data;
 };
 
 export const updateSetting = async (setting, value) => {
-  const response = await api.patch('/settings', { setting, value });
+  const response = await api.patch('/api/settings', { setting, value });
   return response.data;
 };
