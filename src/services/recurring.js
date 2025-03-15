24e5bff56d7322dded033422f85ed5a5ec54fc91
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-03-15 23:30:09 +0530

    Update recurring.js

--- src/services/recurring.js
+++ src/services/recurring.js
@@ -1,21 +1,21 @@
 import api from './api';
 
 export const getRecurringExpenses = async () => {
-  const response = await api.get('/recurring');
+  const response = await api.get('/api/recurring');
   return response.data;
 };
 
 export const scheduleRecurringExpense = async (recurringData) => {
-  const response = await api.post('/recurring', recurringData);
+  const response = await api.post('/api/recurring', recurringData);
   return response.data;
 };
 
 export const updateRecurring = async (id, recurringData) => {
-  const response = await api.put(`/recurring/${id}`, recurringData);
+  const response = await api.put(`/api/recurring/${id}`, recurringData);
   return response.data;
 };
 
 export const deleteRecurring = async (id) => {
-  const response = await api.delete(`/recurring/${id}`);
+  const response = await api.delete(`/api/recurring/${id}`);
   return response.data;
 };
