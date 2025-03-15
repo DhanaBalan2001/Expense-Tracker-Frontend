f6099c1556af0ddf1fbf0d16d7c5ff03cceb402b
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-03-15 23:28:27 +0530

    Update budget.js

--- src/services/budget.js
+++ src/services/budget.js
@@ -1,22 +1,22 @@
 import api from './api';
 
 export const getBudgets = async () => {
-  const response = await api.get('/budgets');
+  const response = await api.get('/api/budgets');
   return response.data;
 };
 
 export const createBudget = async (budgetData) => {
-  const response = await api.post('api/budgets', budgetData);
+  const response = await api.post('/api/budgets', budgetData);
   return response.data;
 };
 
 export const updateBudget = async (id, budgetData) => {
-  const response = await api.put(`api/budgets/${id}`, budgetData);
+  const response = await api.put(`/api/budgets/${id}`, budgetData);
   return response.data;
 };
 
 export const deleteBudget = async (id) => {
-  const response = await api.delete(`api/budgets/${id}`);
+  const response = await api.delete(`/api/budgets/${id}`);
   return response.data;
 };
 
